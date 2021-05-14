811bf12f0606274de5385247da2997b980a0b1b8
Artur Chmura <achmura66@gmail.com>
2021-05-14 02:40:40 +0200

    unused usings

--- Resolution/Resolution/Visitors/ChildVisitor/ChildVisitor.cs
+++ Resolution/Resolution/Visitors/ChildVisitor/ChildVisitor.cs
@@ -1,9 +1,5 @@
 ﻿using Resolution.Sentences;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Resolution.Visitors
 {
