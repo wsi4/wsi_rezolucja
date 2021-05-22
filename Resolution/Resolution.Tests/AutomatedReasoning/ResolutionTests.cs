48208b95494570ce22acf66fc4d292f6ab3bd24f
Jan Kupis <janekkupis@gmail.com>
2021-05-22 17:20:55 +0200

    Add the resolution method

---
+++ Resolution/Resolution.Tests/AutomatedReasoning/ResolutionTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Resolution.Sentences;
+using System.Collections.Generic;
+
+namespace Resolution.Tests.AutomatedReasoningTests
+{
+    [TestClass]
+    public class ResolutionTests
+    {
+        [TestMethod]
+        public void TestGoodExample()
+        {
+            Sentence headacheDiagnosis = new ComplexSentence(Connective.OR, new Literal("flu"), new Literal("chronic_fatigue"));
+            Sentence soreThroatDiagnosis = new ComplexSentence(Connective.OR, new Literal("flu"), new Literal("chronic_fatigue"));
+
+            Sentence headacheImplication = new ComplexSentence(Connective.IMPLICATION, new Literal("headache"), headacheDiagnosis);
+            Sentence soreThroatImplication = new ComplexSentence(Connective.IMPLICATION, new Literal("sore_throat"), soreThroatDiagnosis);
+            Sentence musclePainImplication = new ComplexSentence(Connective.IMPLICATION, new Literal("muscle_pain"), new Literal("flu"));
+            Sentence musclePainNotImplication = new ComplexSentence(Connective.IMPLICATION, new Literal("sore_throat"), new Literal("chronic_fatigue", true));
+
+            Sentence headache = new Literal("headache");
+            Sentence soreThroat = new Literal("sore_throat");
+            Sentence musclePain = new Literal("muscle_pain");
+            var kb = new List<Sentence>
+            {
+                headacheDiagnosis,
+                soreThroatDiagnosis,
+                headacheImplication,
+                soreThroatImplication,
+                musclePainImplication,
+                musclePainNotImplication,
+                headache,
+                soreThroat,
+                musclePain
+            };
+
+
+            var result = AutomatedReasoning.Resolution(kb, new Literal("flu"));
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestBadExample()
+        {
+            Sentence headacheDiagnosis = new ComplexSentence(Connective.OR, new Literal("flu"), new Literal("chronic_fatigue"));
+            Sentence soreThroatDiagnosis = new ComplexSentence(Connective.OR, new Literal("flu"), new Literal("chronic_fatigue"));
+
+            Sentence headacheImplication = new ComplexSentence(Connective.IMPLICATION, new Literal("headache"), headacheDiagnosis);
+            Sentence soreThroatImplication = new ComplexSentence(Connective.IMPLICATION, new Literal("sore_throat"), soreThroatDiagnosis);
+            Sentence musclePainImplication = new ComplexSentence(Connective.IMPLICATION, new Literal("muscle_pain"), new Literal("flu"));
+            Sentence musclePainNotImplication = new ComplexSentence(Connective.IMPLICATION, new Literal("sore_throat"), new Literal("chronic_fatigue", true));
+
+            Sentence headache = new Literal("headache");
+            Sentence soreThroat = new Literal("sore_throat");
+            Sentence musclePain = new Literal("muscle_pain");
+            var kb = new List<Sentence>
+            {
+                headacheDiagnosis,
+                soreThroatDiagnosis,
+                headacheImplication,
+                soreThroatImplication,
+                musclePainImplication,
+                musclePainNotImplication,
+                headache,
+                soreThroat,
+                musclePain
+            };
+
+
+            var result = AutomatedReasoning.Resolution(kb, new Literal("chronic_fatigue"));
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ Resolution/Resolution/AutomatedReasoning/AutomatedReasoning.cs
@@ -0,0 +1,100 @@
+﻿using Resolution.Clauses;
+using Resolution.Sentences;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Resolution
+{
+    public static class SetExtensions
+    {
+        public static bool ContainsAll<T>(this ISet<T> set, IEnumerable<T> enumerable)
+        {
+            return enumerable.All(e => set.Contains(e));
+        }
+    }
+
+    public static class AutomatedReasoning
+    {
+        public static bool Resolution(IEnumerable<Sentence> kb, Sentence alpha)
+        {
+            var cNFConverter = new CNFConverter();
+
+            var alphaClone = (Sentence)alpha.Clone();
+            alphaClone.Negate();
+
+            var clausesSet = kb.Append(alphaClone)
+                .Aggregate(Enumerable.Empty<Clause>(), (acc, current) => acc.Concat(cNFConverter.ConvertToCNF(current))).ToHashSet();
+            var newClauses = new List<Clause>();
+
+            do
+            {
+                var clausesList = clausesSet.ToList();
+
+                for (int i = 0; i < clausesList.Count; i++)
+                {
+                    var ci = clausesList[i];
+                    for (int j = i + 1; j < clausesList.Count; j++)
+                    {
+                        Clause cj = clausesList[j];
+
+                        var resolvents = Resolve(ci, cj);
+                        if (resolvents.Any(c => c.Empty))
+                        {
+                            return true;
+                        }
+
+                        newClauses.AddRange(resolvents);
+                    }
+                }
+
+                if (clausesSet.ContainsAll(newClauses))
+                {
+                    return false;
+                }
+
+                clausesSet.UnionWith(newClauses);
+
+            } while (true);
+        }
+
+        private static ISet<Clause> Resolve(Clause ci, Clause cj)
+        {
+            var resolvents = ResolvePositiveWithNegative(ci, cj);
+            resolvents.UnionWith(ResolvePositiveWithNegative(cj, ci));
+
+            return resolvents;
+        }
+
+        private static ISet<Clause> ResolvePositiveWithNegative(Clause c1, Clause c2)
+        {
+            var complementary = c1.PositiveLiterals.Select(p => p.Symbol)
+                .Intersect(c2.NegativeLiterals.Select(p => p.Symbol));
+            var resolvents = new HashSet<Clause>();
+
+            foreach (var complement in complementary)
+            {
+                var resolventLiterals = new List<Literal>();
+
+                foreach (var c1l in c1.Literals)
+                {
+                    if (c1l.Negated || !(c1l.Symbol == complement))
+                    {
+                        resolventLiterals.Add(c1l);
+                    }
+                }
+
+                foreach (var c2l in c2.Literals)
+                {
+                    if (!c2l.Negated || !(c2l.Symbol == complement))
+                    {
+                        resolventLiterals.Add(c2l);
+                    }
+                }
+
+                resolvents.Add(new Clause(resolventLiterals));
+            }
+
+            return resolvents;
+        }
+    }
+}
